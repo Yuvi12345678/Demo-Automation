e77b3f7d81fe4fb86338457c15b6e81b0e7ee6cc
Yuvi <Yuvi@Yuvi_Shinde>
2022-08-26 02:37:30 -0700

    yuvraj

---
+++ Yuvraj/src/Arrays/example1.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		
+		String arr[] = new String[5];		
+		
+		arr[0] = "Yuvraj";
+		arr[1] = "Smita";
+		arr[2] = "Dyaneshwar";
+		arr[3] = "Prashant";
+		arr[4] = "Mohini";
+		
+		for(int i=0; i<=4; i++) {
+			System.out.print(arr[i]+" "); //when we want info in row then use print
+			
+			//System.out.println(arr[i]); // when we want info in coloumn then we use println
+			
+			
+		}
+		
+	}
+}
